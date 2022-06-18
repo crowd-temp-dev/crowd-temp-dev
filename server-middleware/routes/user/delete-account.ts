fb5d8e2ade13a1397b67791cad2ed64e4d5304f7
chidi-beeboard <c4benni@gmail.com>
2022-06-18 22:09:33 +0100

    feat: deploy

--- server-middleware/routes/user/delete-account.ts
+++ server-middleware/routes/user/delete-account.ts
@@ -57,9 +57,9 @@ export default function (router: Router) {
           })
 
           if (user) {
-            if (process.env.STAGING === '1') {
+            if (process.env.IS_STAGING === '1') {
               if (/^fakeuser(?:-\d)?@/.test(user.email)) {
-                throw new Error('{403} You Cant delete dummy account!')
+                throw new Error('{403} You Cant delete a dummy account!')
               }
             }
 
