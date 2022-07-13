dbd7fc1fe9bc94f11c72846120908d88bb8d5f99
chidi-beeboard <c4benni@gmail.com>
2022-07-13 20:29:36 +0100

    feat: deploy

--- components/Base/HeaderLogo/index.vue
+++ components/Base/HeaderLogo/index.vue
@@ -17,7 +17,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <h1 class="text-[20.07px] leading-[20.57px] font-medium font-epilogue">
+  <h1 class="text-[20.07px] leading-[20.57px] font-medium font-epilogue transition-opacity active:opacity-70">
     <NuxtLink
       to="/"
       class="inline-flex items-end"
