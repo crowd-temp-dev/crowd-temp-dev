09fa1714ea8c5a42d3b7e2ecef5a87198922376a
chidi-beeboard <c4benni@gmail.com>
2022-07-14 22:25:33 +0100

    feat: deploy

--- components/LandingPage/Footer/index.vue
+++ components/LandingPage/Footer/index.vue
@@ -1,20 +1,43 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'LandingPageFooter',
+  components: {},
+  setup() {
+    const scrollToTop = () => {
+      scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+    }
+
+    return { scrollToTop }
+  },
+})
+</script>
+
 <template>
   <footer class="h-78 pt-24 pb-40 bg-surface-default shadow-2">
     <section
       class="max-w-[100rem] mx-auto flex justify-center md:justify-between items-start"
     >
-      <h2 class="text-[20.07px] leading-[20.57px] font-medium hidden md:flex items-center">
-        <NuxtLink to="/" class="inline-flex items-center">
-          <Img
-            src="static/logo"
-            alt="App logo"
-            :width="22.26"
-            :height="16"
-            class="h-16 w-[22.26px] mr-2"
-          />
+      <h2
+        class="text-[20.07px] leading-[20.57px] font-medium hidden md:flex items-center"
+      >
+        <span class="transition-opacity active:opacity-70" @click="scrollToTop">
+          <NuxtLink to="/" class="inline-flex items-center">
+            <Img
+              src="static/logo"
+              alt="App logo"
+              :width="22.26"
+              :height="16"
+              class="h-16 w-[22.26px] mr-2"
+            />
 
-          crowd
-        </NuxtLink>
+            crowd
+          </NuxtLink>
+        </span>
 
         <span class="text-button ml-10">
           Built with ❤️ by
@@ -22,7 +45,7 @@
             href="https://www.unbugqa.com"
             target="_blank"
             rel="noreferrer noopener"
-            class="text-action-primary-default"
+            class="text-action-primary-default hover:underline hover:text-action-primary-hovered active:text-action-primary-pressed"
             >UnbugQA</a
           >
         </span>
@@ -32,15 +55,3 @@
     </section>
   </footer>
 </template>
-
-<script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-  name: 'LandingPageFooter',
-  components: {},
-  setup() {},
-})
-</script>
-
-<style scoped></style>
