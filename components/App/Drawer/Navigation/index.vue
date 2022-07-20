866e2c55dec0cc3a762135e7ded416c51134895a
chidi-beeboard <c4benni@gmail.com>
2022-07-20 13:40:03 +0100

    feat: deploy

--- components/App/Drawer/Navigation/index.vue
+++ components/App/Drawer/Navigation/index.vue
@@ -184,7 +184,7 @@ export default defineComponent({
             v-slot="{ events }"
             :label="link.title"
             placement="right"
-            content-class="!text-body !rounded-[6px] !py-6 !px-12"
+            content-class="!text-body !rounded-[6px] !py-6 !px-12 !font-medium"
             arrow-size="7px"
           >
             <span v-on="events">
