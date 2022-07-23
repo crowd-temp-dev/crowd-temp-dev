771aa25b3b370a198049698bbb4c33b65c278d49
chidi-beeboard <c4benni@gmail.com>
2022-07-23 13:23:47 +0100

    feat: deploy

---
+++ components/Base/DynamicTag/index.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'DynamicTag',
+  props: {
+    tag: {
+      type: String as () => keyof HTMLElementTagNameMap,
+      required: true,
+    },
+  },
+  render(h) {
+    return h(
+      this.tag,
+      {
+        attrs: {
+          ...this.$attrs,
+        },
+        on: {
+          ...this.$listeners,
+        },
+      },
+      this.$slots.default
+    )
+  },
+})
+</script>
